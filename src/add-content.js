8b89e9213e7385a621eb9a0f98e2a9c0fe58fd61
laoluo <375119293@qq.com>
2021-03-16 22:32:53 +0800

    fix: update ignore

--- src/add-content.js
+++ src/add-content.js
@@ -1,3 +1,5 @@
+import _ from 'lodash/isArray';
+
 export default function() {
     document.write('Hello World')
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+const path = require('path')
+
 module.exports = {
+    context: path.join(__dirname, './src'),
     entry: './src/index.js',
     output: {
         filename: 'bundle.js'
